2ed85b9d2caf8fec560efed11f52b8e2bb88e5c9
DZultra <DanielZ123@web.de>
2026-01-18 22:08:23 +0100

    Added some basic utility code

--- src/main/java/net/dzultra/hytaleplugin/HytalePlugin.java
+++ src/main/java/net/dzultra/hytaleplugin/HytalePlugin.java
@@ -1,12 +1,19 @@
 package net.dzultra.hytaleplugin;
 
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import net.dzultra.hytaleplugin.commands.PluginHelloCommand;
 import net.dzultra.hytaleplugin.events.CraftingEvent;
 import net.dzultra.hytaleplugin.events.OnPlayerReadyEvent;
+
+import java.util.Collection;
 
 public class HytalePlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
@@ -21,5 +28,12 @@ public class HytalePlugin extends JavaPlugin {
         this.getCommandRegistry().registerCommand(new PluginHelloCommand(this.getName(), this.getManifest().getVersion().toString()));
         this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, OnPlayerReadyEvent::onPlayerReady);
         this.getEntityStoreRegistry().registerSystem(new CraftingEvent());
+
+        Universe universe = Universe.get();
+        World world = universe.getWorlds().get("default");
+
+        Store<EntityStore> store = world.getEntityStore().getStore();
+
+        Collection<PlayerRef> players = world.getPlayerRefs();
     }
 }
