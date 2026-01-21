54af3b08cd5d1e7d41a5b61922f36cd3db4e3bcc
DZultra <DanielZ123@web.de>
2026-01-21 21:08:31 +0100

    Moved Examples to dedicated class

--- src/main/java/net/dzultra/hytaleplugin/HytalePlugin.java
+++ src/main/java/net/dzultra/hytaleplugin/HytalePlugin.java
@@ -23,46 +23,5 @@ public class HytalePlugin extends JavaPlugin {
         this.getCommandRegistry().registerCommand(new TestCommand());
         this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, OnPlayerReadyEvent::onPlayerReady);
         this.getEntityStoreRegistry().registerSystem(new CraftingEvent());
-
-//        Universe universe = Universe.get();
-//
-//        PlayerRef player = universe.getPlayerByUsername("DZultra", NameMatching.EXACT);
-//        World world = universe.getWorlds().get("default");
-//
-//        Store<EntityStore> store = world.getEntityStore().getStore();
-//
-//        // Pattern: Get the ref, Get the store, Get the Component, Read the Data
-//
-//        if (player != null) {
-//            TransformComponent transformComponent = store.getComponent(player.getReference(), TransformComponent.getComponentType());
-//            Vector3d position = transformComponent.getPosition();
-//
-//            store.addComponent(player.getReference(), Invulnerable.getComponentType());
-//
-//            transformComponent.setPosition(new Vector3d(1, 1, 1));
-//            transformComponent.setPosition(position); // Just examples so we don't have any issues hehe
-//
-//            store.removeComponent(player.getReference(), Invulnerable.getComponentType());
-//
-//            Teleport teleport = new Teleport(new Vector3d(0, 0, 0), player.getHeadRotation());
-//            store.addComponent(player.getReference(), Teleport.getComponentType(), teleport);
-//
-//            // No own components for health etc. uses a statscomponent
-//            EntityStatMap stats = store.getComponent(player.getReference(), EntityStatMap.getComponentType());
-//            int healthIndex = DefaultEntityStatTypes.getHealth();
-//            EntityStatValue health = stats.get(healthIndex);
-//            float current = health.getMax();
-//            float max = health.get();
-//
-//            stats.addStatValue(healthIndex, 0);
-//            stats.addStatValue(healthIndex, -0);
-//        }
-//
-//        Collection<PlayerRef> players = world.getPlayerRefs();
-//
-//        for (PlayerRef playerRef : players) {
-//            if (!playerRef.getUsername().equals("DZultra")) return;
-//
-//        }
     }
 }
